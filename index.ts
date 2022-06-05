0e1324c75f831edab1906917fa0d6567cd8be97b
Kevin Lee <kevinlee3618@gmail.com>
2022-06-05 11:02:18 -0700

    add interface

--- index.ts
+++ index.ts
@@ -1,7 +1,30 @@
 import axios from 'axios';
+import { copyFileSync } from 'fs';
 
-const url = 'https://jsonplaceholder.typicode.com/todos';
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 axios.get(url).then(response => {
   console.log(response.data);
+
+  const todo = response.data as Todo;
+
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  logTodo(id, title, completed);
 });
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+    The ToDo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished?: ${completed}
+  `);
+}
